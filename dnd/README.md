4b1913dd09ab24bbfbf7da2a57fd70089fb274ed
modorigoon <modorigoon@gmail.com>
2022-09-06 00:24:16 +0900

    add: React beautiful dnd examples

---
+++ dnd/README.md
@@ -0,0 +1,15 @@
+# React Beautiful DnD Example
+
+## Examples
+```
+ - React beautiful Dnd
+```
+## Fill-in node_modules
+```
+npm install
+```
+
+## Run
+```
+npm start
+```
---
+++ dnd/src/App.tsx
@@ -0,0 +1,32 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd"
+
+function App() {
+  const onDragEnd = () => { };
+
+  return <DragDropContext onDragEnd={onDragEnd}>
+    <div>
+      <Droppable droppableId="one">
+        {(magic) =>
+          <ul ref={magic.innerRef} {...magic.droppableProps}>
+            <Draggable draggableId="first" index={0}>
+              {(magic) =>
+                <li ref={magic.innerRef} {...magic.draggableProps}>
+                  <span {...magic.dragHandleProps}>[MOVE]</span>ONE
+                </li>
+              }
+            </Draggable>
+            <Draggable draggableId="second" index={1}>
+              {(magic) =>
+                <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                  TWO
+                </li>
+              }
+            </Draggable>
+          </ul>
+        }
+      </Droppable>
+    </div>
+  </DragDropContext>
+}
+
+export default App;
